13acaac864dce28da9431c67b8aa491aef9ba434
Bor <Bor_85@inbox.ru>
2026-04-02 11:11:15 +0700

    Обновил удаленные файлы и добавил новые

--- java-ShipIt/src/StandardParcel.java
+++
@@ -1,7 +0,0 @@
-public class StandardParcel extends Parcel {
-    private static final int DEFAULT_BASIC_COST = 2;
-
-    public StandardParcel(String description, int weight, String deliveryAddress, int sendDay) {
-        super(description, weight, deliveryAddress, sendDay, DEFAULT_BASIC_COST);
-    }
-}
--- java-ShipIt/src/Trackable.java
+++
@@ -1,3 +0,0 @@
-public interface Trackable {
-    void reportStatus(String newLocation);
-}
